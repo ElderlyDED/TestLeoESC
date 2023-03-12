2a13f6a37dd6f21da1a7f170d81d753e56d28c8d
Elderly <elderlyinside@gamil.com>
2023-03-12 20:36:21 +0300

    Была реализована система аутивации партиклов с помощью ECS

---
+++ Assets/ECS/Providers/PlayerParticleProvider.cs
@@ -0,0 +1,4 @@
+using Voody.UniLeo;
+using UnityEditor;
+
+public class PlayerParticleProvider : MonoProvider<PlayerParticleComponent>{}
--- Assets/ECS/Systems/InputPlayerSystem.cs
+++ Assets/ECS/Systems/InputPlayerSystem.cs
@@ -5,9 +5,10 @@ using Leopotam.Ecs;
 
 sealed class InputPlayerSystem : IEcsRunSystem
 {
-    readonly EcsFilter<PlayerTag, DirectionComponent> _directionFilter = null;
+    readonly EcsFilter<PlayerTag, DirectionComponent, PlayerParticleComponent> _directionFilter = null;
     float _moveX;
     float _moveZ;
+    bool _onParticleSystem;
     public void Run()
     {
         SetDirection();
@@ -18,11 +19,33 @@ sealed class InputPlayerSystem : IEcsRunSystem
             direction.x = _moveX;
             direction.z = _moveZ;   
         }
+        if (Input.GetKey(KeyCode.P))
+        {
+            foreach (var i in _directionFilter)
+            {
+                StartParticleSystem(_directionFilter.Get3(i).particleSystem);
+            }
+        }
+        
     }
 
     void SetDirection()
     {
         _moveX = Input.GetAxis("Horizontal");
         _moveZ = Input.GetAxis("Vertical");
+    }
+
+    void StartParticleSystem(ParticleSystem playerParticleSystem)
+    {
+        if (_onParticleSystem == false)
+        {
+            _onParticleSystem = true;
+            playerParticleSystem.Play(); 
+        }
+        else
+        {
+            _onParticleSystem = false;
+            playerParticleSystem.Stop();
+        }
     }
 }
